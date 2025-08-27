c6d44f2847f70e3cccd0a9bd5d1d28ec76521f34
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-08-27 16:26:00 +0700

    feat(test): test

--- bin/index.js
+++ bin/index.js
@@ -29,17 +29,17 @@
 //     console.log(strings.join(options.separator));
 //   });
 //
-// // TODO: list
-// // get the args
-// // if arg is "." then get all file and folder in the current dir
-// // if arg is specific file/dir then only symlink that file/dir
-// // if using --target option then use that folder as a target otherwise then use home dir
-// // google on what's the needed requirement to symlink file
-// // -s command to symlink
-// // -d command to delete symlink
-// // -r command to delete and then do another symlink
-//
 // program.parse()
+
+// TODO: list
+// get the args
+// if arg is "." then get all file and folder in the current dir
+// if arg is specific file/dir then only symlink that file/dir
+// if using --target option then use that folder as a target otherwise then use home dir
+// google on what's the needed requirement to symlink file
+// -s command to symlink
+// -d command to delete symlink
+// -r command to delete and then do another symlink
 
 const process = require('process')
 
@@ -47,14 +47,12 @@ const availableCommand = {
   'option': {
     '--start': {
       'description': "to start the app",
-      // start: (data) => {
       command: function (data) {
         return data == null || data == undefined ? "starting the app" : `starting the app with ${data}`
       }
     },
     '--build': {
       'description': 'to build the app',
-      // build: () => {
       command: function () {
         return "building the app"
       }
@@ -64,7 +62,9 @@ const availableCommand = {
 
 try {
   const getArgument = getArgs(process.argv)
-  const cmd = executeCommand(getArgument)
+  console.log(getArgument)
+  // const cmd = executeCommand(getArgument)
+  // console.log(cmd)
 } catch(err) {
   console.log(err.message)
   console.log(usage())
@@ -79,22 +79,25 @@ function getArgs(input) {
 
   input.splice(0, 2)
   input.map((inp, index) => {
-    if (inp.slice(0, 2) === "--") {
-      let argArray = inp.split("=")
-      let argOption = argArray[0]
-      let argValue = argArray.length > 1 ? argArray[1] : null
-      optionObj[argOption] = argValue
-    } else if (inp[0] === "-") {
-      let flags = inp.slice(1).split("")
-      flags.forEach((flag) => {
-        flagObj[flag] = null
-      })
-    } else {
-      argArr.push(inp)
+    if (inp == "--start") {
+      console.log("start yooo")
     }
+    // if (inp.slice(0, 2) === "--") {
+    //   let argArray = inp.split("=")
+    //   let argOption = argArray[0]
+    //   let argValue = argArray.length > 1 ? argArray[1] : null
+    //   optionObj[argOption] = argValue
+    // } else if (inp[0] === "-") {
+    //   let flags = inp.slice(1).split("")
+    //   flags.forEach((flag) => {
+    //     flagObj[flag] = null
+    //   })
+    // } else {
+    //   argArr.push(inp)
+    // }
   })
-  output["options"] = optionObj
-  output["flags"] = flagObj
+  output["option"] = optionObj
+  output["flag"] = flagObj
   output["args"] = argArr
   return output
 }
@@ -105,18 +108,14 @@ function executeCommand(input) {
 
   for (const [key, value] of Object.entries(input)) {
     for (const [k, v] of Object.entries(value)) {
-      console.log(key, value, k, v)
-      console.log(availableCommand[key][k])
-      // for (const [name, command] of v) {
-      // console.log(key, value, k, v, name, command)
-      // }
-      // if (key == 'args') {
-      //   if (!availableCommand[key].hasOwnProperty(v)) throw new Error(`Unknown or unexpected option: ${v} \n`)
-      //   cmd[k] = availableCommand[key][v](v)
-      // } else {
-      // if (!availableCommand[key].hasOwnProperty(k)) throw new Error(`Unknown or unexpected option: ${k} \n`)
-      // cmd[k] = availableCommand[key][k](v)
-      // }
+      // console.log(availableCommand[key][k].command())
+      if (key == 'args') {
+        if (!availableCommand[key].hasOwnProperty(v)) throw new Error(`Unknown or unexpected option: ${v} \n`)
+        cmd[k] = availableCommand[key][v].command(v)
+      } else {
+        if (!availableCommand[key].hasOwnProperty(k)) throw new Error(`Unknown or unexpected option: ${k} \n`)
+        cmd[k] = availableCommand[key][k].command(v)
+      }
     }
   }
   return Object.values(cmd)
